42be6cd4ad038d28e576b6368a5a55de4e2210d3
vipul-zambare006 <vipulzambare1993@gmail.com>
2019-03-27 23:57:34 +0800

    Added closet assigned functionality

--- src/components/ClosetDeliveryAssignComponent.js
+++ src/components/ClosetDeliveryAssignComponent.js
@@ -1,19 +1,50 @@
 import React, { Component } from "react";
 const api = require("../config/api");
 export default class ClosetDeliveryAssignComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      closetId: ""
+    };
+  }
+
+  isClosetAssignable = () => {
+    const result = this.props.orderItems
+      .map(item => item.status)
+      .includes("Pending");
+
+    if (result) {
+      alert("Closet can not be assigned as some items are in Pending state");
+    } else {
+      this.setState({
+        closetId: "CLO-ZYX-0001"
+      });
+      alert("Closet Assigned Successfully");
+    }
+  };
 
   render() {
     return (
       <div>
         <div>
           <span className="bold-text">Assigned Closet ID:</span>
-            <div className="input-group sm-3 mb-3">
-              <input className="form-input" type="text" />
-              <span>
-                {/* //disable button if any of order item is pending */}
-                <button className="btn btn-primary">Scan Closet ID</button>
-              </span>
-            </div>
+          <div className="input-group sm-3 mb-3">
+            <input
+              readOnly="true"
+              className="form-input"
+              type="text"
+              value={this.state.closetId}
+            />
+            <span>
+              <button
+                className="btn btn-primary"
+                onClick={this.isClosetAssignable}
+              >
+                Scan Closet ID
+              </button>
+            </span>
+          </div>
         </div>
         <div>
           <span className="bold-text">Assigned Delivery Man:</span>
